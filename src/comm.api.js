190fd7db431b4fb8101f4e59abe8be7923021917
longjinwen <204084802@qq.com>
2019-12-25 23:48:53 +0800

    update

--- src/comm.api.js
+++ src/comm.api.js
@@ -1,23 +1,23 @@
 exports.getDateTime = function (strType = 'y-m-d h:i:s', time = 0) {
-    let date;
-    date = time === 0 ? new Date() : new Date(time);
-    let y = date.getFullYear();
-    strType = strType.replace(/y/g, y);
-    let m = date.getMonth() + 1;
-    strType = strType.replace(/m/g, m < 10 ? '0' + m : m);
-    let d = date.getDate();
-    strType = strType.replace(/d/g, d < 10 ? '0' + d : d);
-    let h = date.getHours();
-    strType = strType.replace(/h/g, h < 10 ? '0' + h : h);
-    let i = date.getMinutes();
-    strType = strType.replace(/i/g, i < 10 ? '0' + i : i);
-    let s = date.getSeconds();
-    strType = strType.replace(/s/g, s < 10 ? '0' + s : s);
+  let date;
+  date = time === 0 ? new Date() : new Date(time);
+  let y = date.getFullYear();
+  strType = strType.replace(/y/g, y);
+  let m = date.getMonth() + 1;
+  strType = strType.replace(/m/g, m < 10 ? '0' + m : m);
+  let d = date.getDate();
+  strType = strType.replace(/d/g, d < 10 ? '0' + d : d);
+  let h = date.getHours();
+  strType = strType.replace(/h/g, h < 10 ? '0' + h : h);
+  let i = date.getMinutes();
+  strType = strType.replace(/i/g, i < 10 ? '0' + i : i);
+  let s = date.getSeconds();
+  strType = strType.replace(/s/g, s < 10 ? '0' + s : s);
 
-    return strType;
+  return strType;
 
 };
 exports.makeRandom = function () {
-    return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
+  return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
 };
 
--- src/download.js
+++ src/download.js
@@ -12,197 +12,197 @@ Download.mimeTypes = mimeTypes;
 
 
 function Download() {
-    this.list = [];
-    this.downloadDefaultConfig = {meanwhile: 1, dir: ''};
-    this.optionConfig = {};
-    this.defaultOptionConfig = {
-        dir: this.downloadDefaultConfig.dir,
-        fileName: null,
-        autoSuffix: false
-    };
+  this.list = [];
+  this.downloadDefaultConfig = {meanwhile: 1, dir: ''};
+  this.optionConfig = {};
+  this.defaultOptionConfig = {
+    dir: this.downloadDefaultConfig.dir,
+    fileName: null,
+    autoSuffix: false
+  };
 }
 
 Download.makeKey = function () {
-    return commApi.makeRandom() + '_' + commApi.getDateTime().replace(/( |:|-)/g, '') + '_' + new Date().getTime();
+  return commApi.makeRandom() + '_' + commApi.getDateTime().replace(/( |:|-)/g, '') + '_' + new Date().getTime();
 };
 
 Download.download = function (options, hooks = {}) {
-    return new Promise(async (resolve, reject) => {
-        !cache.has('dir') && (cache.set('dir', []));
-        let dirs = cache.get('dir');
-        if (!dirs.includes(options.dir)) {
-            let newDir = await makeDir(options.dir);
-            dirs.push(newDir);
-            cache.set('dir', dirs);
+  return new Promise(async (resolve, reject) => {
+    !cache.has('dir') && (cache.set('dir', []));
+    let dirs = cache.get('dir');
+    if (!dirs.includes(options.dir)) {
+      let newDir = await makeDir(options.dir);
+      dirs.push(newDir);
+      cache.set('dir', dirs);
+    }
+
+    hooks.start && hooks.start();
+
+    try {
+      request(options.url)
+        ['on']('error', (response) => {
+        reject({
+          status: 0,
+          msg: 'Request error: ' + response.code,
+          resource: options.url,
+        });
+        hooks.complete && hooks.complete();
+      })
+        ['on']('response', (response) => {
+
+        let suffix;
+        if (response.headers['content-disposition'] && 'application/octet-stream' === response.headers['content-type']) {
+          suffix = response.headers['content-disposition'].split(';')[1].split('.').pop();
+        } else if ('application/octet-stream' === response.headers['content-type']) {
+          let url = options.url;
+          url = url.includes('?') ? url.split('?')[0] : url;
+          suffix = url.split('.').pop();
+        } else {
+          suffix = mimeTypes['extension'](response.headers['content-type']);
         }
 
-        hooks.start && hooks.start();
+        let fileName = options.fileName || (Download.makeKey() + '.' + suffix);
+        fileName = (options.autoSuffix === true && options.fileName) ? (fileName + '.' + suffix) : fileName;
+        let filePath = path.resolve(options.dir, fileName);
 
-        try {
-            request(options.url)
-                ['on']('error', (response) => {
-                reject({
-                    status: 0,
-                    msg: 'Request error: ' + response.code,
-                    resource: options.url,
-                });
-                hooks.complete && hooks.complete();
-            })
-                ['on']('response', (response) => {
+        if (response.statusCode === 200) {
+          response.pipe(fs.createWriteStream(filePath))
+            .on('close', () => {
 
-                let suffix;
-                if (response.headers['content-disposition'] && 'application/octet-stream' === response.headers['content-type']) {
-                    suffix = response.headers['content-disposition'].split(';')[1].split('.').pop();
-                } else if ('application/octet-stream' === response.headers['content-type']) {
-                    let url = options.url;
-                    url = url.includes('?') ? url.split('?')[0] : url;
-                    suffix = url.split('.').pop();
-                } else {
-                    suffix = mimeTypes['extension'](response.headers['content-type']);
+              resolve({
+                status: 200,
+                msg: 'success',
+                resource: options.url,
+                data: {
+                  filePath: filePath,
+                  contentType: response.headers['content-type'],
                 }
-
-                let fileName = options.fileName || (Download.makeKey() + '.' + suffix);
-                fileName = (options.autoSuffix === true && options.fileName) ? (fileName + '.' + suffix) : fileName;
-                let filePath = path.resolve(options.dir, fileName);
-
-                if (response.statusCode === 200) {
-                    response.pipe(fs.createWriteStream(filePath))
-                        .on('close', () => {
-
-                            resolve({
-                                status: 200,
-                                msg: 'success',
-                                resource: options.url,
-                                data: {
-                                    filePath: filePath,
-                                    contentType: response.headers['content-type'],
-                                }
-                            });
-                            hooks.complete && hooks.complete();
-                        });
-
-                } else {
-                    reject({
-                        status: response.statusCode,
-                        msg: 'Response error: The status code is not 200',
-                        resource: options.url
-                    });
-                    hooks.complete && hooks.complete();
-                }
-
+              });
+              hooks.complete && hooks.complete();
             });
-        } catch (e) {
-            reject({
-                status: 0,
-                msg: 'Request error: ' + e.message,
-                resource: options.url
-            });
-            hooks.complete && hooks.complete();
+
+        } else {
+          reject({
+            status: response.statusCode,
+            msg: 'Response error: The status code is not 200',
+            resource: options.url
+          });
+          hooks.complete && hooks.complete();
         }
 
-    });
+      });
+    } catch (e) {
+      reject({
+        status: 0,
+        msg: 'Request error: ' + e.message,
+        resource: options.url
+      });
+      hooks.complete && hooks.complete();
+    }
+
+  });
 };
 
 
 Download.prototype.exec = function () {
-    return new Promise((resolve) => {
-        if (this.list.length === 0) {
-            resolve([]);
-            return;
+  return new Promise((resolve) => {
+    if (this.list.length === 0) {
+      resolve([]);
+      return;
+    }
+    let data = [],
+      j = 0,
+      i = 0,
+      l = this.list.length,
+      end = () => {
+        if (j === l) {
+          resolve(data);
         }
-        let data = [],
-            j = 0,
-            i = 0,
-            l = this.list.length,
-            end = () => {
-                if (j === l) {
-                    resolve(data);
-                }
+      },
+      next = () => {
+        i++;
+        let option = this.list.splice(0, 1)[0];
+        // console.error(option);
+        return Download.download(
+          {
+            url: option.url,
+            dir: option.dir || this.optionConfig.dir,
+            fileName: option.fileName,
+            autoSuffix: option.autoSuffix,
+          },
+          {
+            complete: () => {
+              j++;
+              if (this.list.length > 0) {
+                i--;
+                exec();
+              }
             },
-            next = () => {
-                i++;
-                let option = this.list.splice(0, 1)[0];
-                // console.error(option);
-                return Download.download(
-                    {
-                        url: option.url,
-                        dir: option.dir || this.optionConfig.dir,
-                        fileName: option.fileName,
-                        autoSuffix: option.autoSuffix,
-                    },
-                    {
-                        complete: () => {
-                            j++;
-                            if (this.list.length > 0) {
-                                i--;
-                                exec();
-                            }
-                        },
-                        start: () => {
-                            if (i <= this.optionConfig.meanwhile - 1 && this.list.length > 0) {
-                                exec();
-                            }
-                        }
-                    }
-                );
-            },
-            exec = () => {
-                next()
-                    .then(res => {
-                        data.push(res);
-                        end();
-                    })
-                    .catch(err => {
-                        data.push(err);
-                        end();
-                    });
-            };
+            start: () => {
+              if (i <= this.optionConfig.meanwhile - 1 && this.list.length > 0) {
+                exec();
+              }
+            }
+          }
+        );
+      },
+      exec = () => {
+        next()
+          .then(res => {
+            data.push(res);
+            end();
+          })
+          .catch(err => {
+            data.push(err);
+            end();
+          });
+      };
 
-        exec();
-    });
+    exec();
+  });
 
 };
 Download.prototype.push = function (options) {
-    switch (Object.prototype.toString.call(options)) {
-        case '[object Array]': {
-            this.list = this.list.concat(options);
-            break;
-        }
-        case '[object String]': {
-            this.list.push(options);
-            break;
-        }
-        case '[object Object]': {
-            this.list.push(options);
-            break;
-        }
+  switch (Object.prototype.toString.call(options)) {
+    case '[object Array]': {
+      this.list = this.list.concat(options);
+      break;
     }
-    this.list.forEach((item, index) => {
-        // 独立配置
+    case '[object String]': {
+      this.list.push(options);
+      break;
+    }
+    case '[object Object]': {
+      this.list.push(options);
+      break;
+    }
+  }
+  this.list.forEach((item, index) => {
+    // 独立配置
 
 
-        switch (Object.prototype.toString.call(item)) {
-            case '[object String]': {
-                this.list[index] = Object.assign(JSON.parse(JSON.stringify(this.defaultOptionConfig)), {url: item});
+    switch (Object.prototype.toString.call(item)) {
+      case '[object String]': {
+        this.list[index] = Object.assign(JSON.parse(JSON.stringify(this.defaultOptionConfig)), {url: item});
 
-                break;
-            }
-            case '[object Object]': {
-                this.list[index] = Object.assign(JSON.parse(JSON.stringify(this.defaultOptionConfig)), item);
-                break;
+        break;
+      }
+      case '[object Object]': {
+        this.list[index] = Object.assign(JSON.parse(JSON.stringify(this.defaultOptionConfig)), item);
+        break;
 
-            }
+      }
 
-        }
+    }
 
 
-    });
+  });
 
-    return this;
+  return this;
 };
 Download.prototype.setConfig = function (config) {
-    this.optionConfig = Object.assign(JSON.parse(JSON.stringify(this.downloadDefaultConfig)), config);
-    return this;
+  this.optionConfig = Object.assign(JSON.parse(JSON.stringify(this.downloadDefaultConfig)), config);
+  return this;
 };
 module.exports = Download;
 
